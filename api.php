8f99a989cd6805bd47c7eb3dcd708b90ab862b15
Kostas Kayioulis <kostas@kayioulis.com>
2020-03-16 04:36:30 +0200

    Formatted Numbers

--- api.php
+++ api.php
@@ -8,6 +8,10 @@ function pascalCase($string, $dontStrip = []){
     return ucwords(str_replace(' ', '', ucwords(preg_replace('/^a-z0-9'.implode('',$dontStrip).']+/', ' ', str_replace('.', '', $string)))));
 }
 
+function formatNums($string) {
+    return intval(str_replace('+', '', str_replace(',', '', trim($string))));
+}
+
 $dom = new Dom;
 $dom->loadFromUrl('https://www.worldometers.info/coronavirus/');
 $rows = $dom->find('#main_table_countries')->find('tbody')[0]->find('tr');
@@ -22,13 +26,13 @@ foreach ($rows as $row) {
     if ($country == "") $country = trim($cols[0]->find('span')[0]->text);
     $country = str_replace('&eacute;', 'e', $country);
     $c_array['country'] = $country;
-    $c_array['total_cases'] = trim($cols[1]->text);
-    $c_array['new_cases'] = trim($cols[2]->text);
-    $c_array['total_deaths'] = trim($cols[3]->text);
-    $c_array['new_deaths'] = trim($cols[4]->text);
-    $c_array['total_recovered'] = trim($cols[5]->text);
-    $c_array['active_cases'] = trim($cols[6]->text);
-    $c_array['critical'] = trim($cols[7]->text);
+    $c_array['total_cases'] = formatNums($cols[1]->text);
+    $c_array['new_cases'] = formatNums($cols[2]->text);
+    $c_array['total_deaths'] = formatNums($cols[3]->text);
+    $c_array['new_deaths'] = formatNums($cols[4]->text);
+    $c_array['total_recovered'] = formatNums($cols[5]->text);
+    $c_array['active_cases'] = formatNums($cols[6]->text);
+    $c_array['critical'] = formatNums($cols[7]->text);
     $array[pascalCase($country)] = $c_array;
 }
 
